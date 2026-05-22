067e2aa55572cf55bdc63cc8019634a59f0864b2
CostinDragos <costindragos01@icloud.com>
2026-05-22 14:06:06 +0300

    Removed trailing commas in manifest to provent build errors for Swift 6.0

--- Package.swift
+++ Package.swift
@@ -6,26 +6,26 @@ import PackageDescription
 let package = Package(
   name: "ContextualRotation",
   platforms: [
-    .iOS(.v16),
+    .iOS(.v16)
   ],
   products: [
     // Products define the executables and libraries a package produces, making them visible to
     // other packages.
     .library(
       name: "ContextualRotation",
-      targets: ["ContextualRotation"],
-    ),
+      targets: ["ContextualRotation"]
+    )
   ],
   targets: [
     // Targets are the basic building blocks of a package, defining a module or a test suite.
     // Targets can depend on other targets in this package and products from dependencies.
     .target(
-      name: "ContextualRotation",
+      name: "ContextualRotation"
     ),
     .testTarget(
       name: "ContextualRotationTests",
-      dependencies: ["ContextualRotation"],
-    ),
+      dependencies: ["ContextualRotation"]
+    )
   ],
-  swiftLanguageModes: [.v6],
+  swiftLanguageModes: [.v6]
 )
